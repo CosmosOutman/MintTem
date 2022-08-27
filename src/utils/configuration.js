f772dbe037068488382140396d2b64c51be44b6b
CosmosOutman <90654644+CosmosOutman@users.noreply.github.com>
2022-08-27 23:28:25 +0800

    Update configuration.js

--- src/utils/configuration.js
+++ src/utils/configuration.js
@@ -1,17 +1,17 @@
 // Set smart contract based on chain id
 export const CONTRACTADDRESS = {
     1: "",
-    4: "",
+    4: "0x286aa6e8f1620c218ee2a334a7d6312649394d08",
 };
 
 // Set the price of your NFT
 export const PRICE = 0;
 
 // Set the max amount of your NFT collection
-export const MAX_AMOUNT = 1000;
+export const MAX_AMOUNT = 10;
 
 // Set the max batch size per wallet 
-export const MAX_BATCH_SIZE = 9;
+export const MAX_BATCH_SIZE = 10;
 
 // Set the opesnea url 
-export const OPENSEA_URL = "https://opensea.io/collection/"
+export const OPENSEA_URL = "https://testnets.opensea.io/zh-CN/collection/cosmostest-v4"
